03a341f4e11e362a3a72ee926942dab1b247c5ef
Luis C. Gallego <tostadora@gmail.com>
2025-01-03 14:59:46 +0000

    implemented the Ray struct

---
+++ src/ray.rs
@@ -0,0 +1,26 @@
+use crate::space::{Vec3, Point3};
+
+pub struct Ray {
+    origin: Point3,
+    direction: Vec3,
+}
+
+impl Ray {
+
+    pub fn new(origin: Point3, direction: Vec3) -> Ray {
+        Ray {origin: origin,
+            direction: direction}
+    }
+
+    pub fn origin(self: &Self) -> &Point3 {
+        &self.origin
+    }
+
+    pub fn direction(self: &Self) -> &Vec3 {
+        &self.direction
+    }
+
+    pub fn at(self: &Self, t: f64) -> Point3 {
+        self.origin() + self.direction() * t
+    }
+}
