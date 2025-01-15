ccd640496914e362a7f1da520ef0c64c35e7666f
Luis C. Gallego <tostadora@gmail.com>
2025-01-15 15:26:13 +0000

    fix acne and discard too close hit of rays in the diffusion

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
     // World
 
     let mut world: World = vec!();
-    world.push(Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5));
+    world.push(Sphere::new(Point3::new(0.0, 0.0, -2.0), 0.5));
     world.push(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0));
 
     // Camera
@@ -27,6 +27,7 @@ fn main() {
     camera.aspect_ratio = 16.0 / 9.0;
     camera.image_width = 400;
     camera.samples_per_pixel = 100;
+    camera.max_depth = 50;
     
     camera.render(&world);
 }
