d48f14e510647c4676e909b8d9765750b478bd40
Luis C. Gallego <tostadora@gmail.com>
2025-01-15 15:50:16 +0000

    convert color to gamma space

--- src/color.rs
+++ src/color.rs
@@ -8,10 +8,18 @@ impl Color {
         
         let intensity = Interval::new(0.000, 0.999);
 
-        let r = intensity.clamp(self.x()) * 256.0;
-        let g = intensity.clamp(self.y()) * 256.0;
-        let b = intensity.clamp(self.z()) * 256.0;
+        let r = intensity.clamp(Self::linear_to_gamma(self.x())) * 256.0;
+        let g = intensity.clamp(Self::linear_to_gamma(self.y())) * 256.0;
+        let b = intensity.clamp(Self::linear_to_gamma(self.z())) * 256.0;
 
         println!("{} {} {}", r.floor(), g.floor(), b.floor());
+    }
+
+    fn linear_to_gamma(value: f64) -> f64 {
+        if value > 0.0 {
+            value.sqrt()
+        } else {
+            0.0
+        }
     }
 }
