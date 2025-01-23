7350df43549a3d6851c6645b4eb7eebf196b7f6f
Luis C. Gallego <tostadora@gmail.com>
2025-01-23 15:03:54 +0000

    finished section 6.2. Simplify sphere hit code

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,15 @@ use ray::Ray;
 fn hit_sphere(center: Point3, radius: f64, r: &Ray) -> f64 {
     let oc = &center - &r.origin;
 
-    let a = Vec3::dot(&r.direction, &r.direction);
-    let b = -2.0 * Vec3::dot(&r.direction, &oc);
-    let c = Vec3::dot(&oc, &oc) - radius*radius;
-    let discriminant = b*b - 4.0*a*c;
+    let a = r.direction.length_squared();
+    let h = Vec3::dot(&r.direction, &oc);
+    let c = oc.length_squared() - radius*radius;
+    let discriminant = h*h - a*c;
 
     if discriminant < 0.0 {
         -1.0
     } else {
-        (-b-discriminant.sqrt()) / (2.0 * a)
+        (h-discriminant.sqrt()) / a
     }
 }
 
