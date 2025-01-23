17934700c06747f8e232e38f000160dc1ac3be88
Luis C. Gallego <tostadora@gmail.com>
2025-01-23 13:17:35 +0000

    implemented ection 4.1. Add a ray struct.

---
+++ src/ray.rs
@@ -0,0 +1,15 @@
+use crate::vec3::{Vec3, Point3};
+
+pub struct Ray {
+    pub origin: Point3,
+    pub direction: Vec3,
+}
+
+impl Ray {
+
+    fn at(&self, t: f64) -> Point3 {
+        self.origin + t * self.direction
+    }
+
+}
+
